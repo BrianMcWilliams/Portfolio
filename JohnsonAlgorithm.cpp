6eb0085bcfc41f1048d00697f57d00e4e428d73d
Brian-McW <x-torn-x@hotmail.com>
2015-08-06 16:38:56 -0400

    Johnson algorithm implementation
    This is a C++ implementation of the Johnson algorithm, applied to the
    flow shop or job shop problem where there is only one processing centre
    per machine.

--- JohnsonAlgorithm.cpp
+++ JohnsonAlgorithm.cpp
@@ -1,21 +1,21 @@
-/*
+/*********Job Shop Scheduling************
+*
 *	Author:   Brian McWilliams
 *
-*	Abstract : Implementation of the Johnson algorithm for 2 machine flow - shop where each machine only has one operating centre
+*	Abstract: Implementation of the Johnson algorithm for 2 machine flow-shop where each machine only has one operating centre
 *
 *	The algorithm logic is the following :
-*1 : Find the job with the smallest processing time(on either machine)
-* 2 : If the smallest processing time is on M1->Schedule that job at the beginning
-*	   Else->Schedule that job at the END of the schedule(This case has the processing time being inferior on M2)
-* 3 : If there are unscheduled jobs go to step 1
+*	1: Find the job with the smallest processing time (on either machine)
+*	2: If the smallest processing time is on M1 -> Schedule that job at the beginning
+*	   Else -> Schedule that job at the END of the schedule (This case has the processing time being inferior on M2)
+*	3: If there are unscheduled jobs go to step 1
 *
-*	   To reduce the complexity I opted to sort the table of jobs initially(using quicksort), preventing the need to constantly go through the job table to find the minimum, I would simply
-*	   order them initially and sequentially add them to my schedule with the johnson algorithm.I only sort by minimum time, so if several jobs have the same time on say machine one but different times on machine two they won't be sorted in any
-*	   particular order, this is compliant, albeit naively, with the rules established above.
+*	   To reduce the complexity I opted to sort the table of jobs initially, preventing the need to constantly go through the job table to find the minimum, I would simply
+*	   order them initially and sequentially add them to my schedule with the johnson algorithm.
 *
 *
 *	Note: I'm taking for granted that the user will be inputting the jobs himself, I didn't think too much about input simply because the main goal here is the correct
-*		  Implementation of the lagorithm and not necessarily the fluff around it.I'll be using two tables, one to contain the jobs initially and one to contain the order of the jobs after
+*		  Implementation of the lagorithm and not necessarily the fluff around it. I'll be using two tables, one to contain the jobs initially and one to contain the order of the jobs after
 *		  applying the algorithm
 *
 *****************************************/
@@ -29,11 +29,11 @@ struct job{
 	int t1; //Time to execute on the first machine
 	int t2; //Time to execute on the second machine
 };
-int inputJobs(job* jobTable, int nbJobs){
-
+int inputJobs(job* jobTable,int nbJobs){
+	
 	int time; //Variable used for input
 
-
+	
 	for (int i = 0; i < nbJobs; i++){ //For every job, input the processing time on M1, M2
 
 		cout << "For job " << i << "please enter t1 : ";  //Ask for t1
@@ -42,6 +42,10 @@ int inputJobs(job* jobTable, int nbJobs){
 		cout << endl << "please enter t2 : " << endl; //Ask for t2
 		cin >> time;
 		jobTable[i].t2 = time;
+	}
+	cout << "here are the jobs currently entered" << endl;
+	for (int i = 0; i < nbJobs; i++){ //For every job, input the processing time on M1, M2
+		cout << "Job " << i << "|| t1 : " << jobTable[i].t1 << " t2 : " << jobTable[i].t2 << endl; //Ask for t2
 	}
 	return nbJobs;
 }
@@ -60,7 +64,7 @@ void quickSort(job* jobTable, int left, int right) { //Quicksort algorithm adapt
 	job tmp; //Temporary job to allow us to switch jobs around
 	job* pivot = &jobTable[(left + right) / 2]; // Points to the pivot 
 	min_piv = findMinimum(pivot);
-	while (i <= j) {
+	while (i <= j) { 
 		leftJob = &jobTable[i]; //Initialise leftJob
 		min_cur = findMinimum(leftJob);
 		while (min_cur < min_piv){ //While left jobs are smaller than the pivot value
@@ -92,54 +96,13 @@ void quickSort(job* jobTable, int left, int right) { //Quicksort algorithm adapt
 		quickSort(jobTable, i, right);
 }
 void jobSort(job* jobTable, int length){ //Takes the table and length and calls quicksort
-	quickSort(jobTable, 0, length - 1);
-}
-void scheduleJobs(job* jobTable, job* jobSchedule, int nbJobs){
-
-	job* M2 = NULL; // Create an iterator that will start at the end of the table, ordering the operations that are shortest on t2;
-	job* M1 = NULL; // Create an iterator that will start at the start of the table, ordering the operations that are shortest on t1;
-
-	M1 = &jobSchedule[0]; // Point M1 to the beginning of the table
-	M2 = &jobSchedule[(nbJobs - 1)]; //Point M2 to the end of the table
-	job* currentJob = &jobTable[0]; //Create a pointer that starts at the beginning of the job table 
-	while (currentJob != &jobTable[nbJobs]) { // While M1 and M2 aren't pointing the same element
-		if (currentJob->t1 < currentJob->t2){ // If the time on  the first machine is inferior to the time on the second machine, put it at the start of the schedule
-			M1->t1 = currentJob->t1;
-			M1->t2 = currentJob->t2;
-			M1++; //Increment pointer [ X Y Z ->  . . . L M ] to point to the next empty job slot
-		}
-		else { //Else put it at the end of the schedule
-			M2->t1 = currentJob->t1;
-			M2->t2 = currentJob->t2;
-			M2--; //Decrement pointer [ X Y . . .  <-  N M L ] to point to the next empty job slot
-		}
-		currentJob++;
-	}
-	return;
-}
-int calc_makeSpan(job* jobSchedule, int length){
-	int Tm1 = 0; //Time on machine 1
-	int Tm2 = 0; //Time on machine 2
-	job* current; //Pointer to the current treated job
-	current = &jobSchedule[0];
-
-	for (int j = 0; j < length; j++){
-		Tm1 = Tm1 + current->t1;
-		if (Tm2 > Tm1){
-			Tm2 = Tm2 + current->t2;
-		}
-		else{
-			Tm2 = Tm1 + current->t2;
-		}
-		current++;
-	}
-	return Tm2;
+	quickSort(jobTable,0,length-1);
 }
 
 int main(void){
 
 	/******INPUT*******
-	*
+	* 
 	* I'm going to simply have the user input the number of jobs to treat and then input the comput ation time for each of them.
 	* Once again, not wasting energy on small details regarding how the jobs will be fed into the system, we're focusing on the algorithm
 	*
@@ -150,11 +113,37 @@ int main(void){
 	cout << " ----------------  Thank you  ------------------ " << endl << endl;
 
 	job* jobTable = new job[nbJobs]; //Create a table to house the jobs'
-	nbJobs = inputJobs(jobTable, nbJobs); //Input jobs to the table
-	jobSort(jobTable, nbJobs); //Sort the jobs so that the minimums are in increasing order, regardless of being on t1 t2
-	job* jobSchedule = new job[nbJobs]; //Create a table for the schedule
-	scheduleJobs(jobTable, jobSchedule, nbJobs); //Place jobs from jobTable into jobSchedule
-	cout << "Makespan : " << calc_makeSpan(jobSchedule, nbJobs) << endl;
-	system("pause");
 
+	nbJobs = inputJobs(jobTable,nbJobs);
+	jobSort(jobTable,nbJobs);
+	cout << "here are the jobs currently entered" << endl;
+	for (int i = 0; i < nbJobs; i++){ //For every job, input the processing time on M1, M2
+		cout << "Job " << i << "|| t1 : " << jobTable[i].t1 << " t2 : " << jobTable[i].t2 << endl; //Ask for t2
+	}
+	job* jobSchedule = new job[nbJobs]; //Create a table to set the order
+
+
+	job* M2 = NULL; // Create an iterator that will start at the end of the table, ordering the operations that are shortest on t2;
+	job* M1 = NULL; // Create an iterator that will start at the start of the table, ordering the operations that are shortest on t1;
+
+	M1 = &jobSchedule[0]; // Point M1 to the beginning of the table
+	M2 = &jobSchedule[nbJobs]; //Point M2 to the end of the table
+	M2--;
+
+	job* currentJob = &jobTable[0]; //Create a pointer that starts at the beginning of the job table 
+	bool loop = true;
+	while (loop) { // While M1 and M2 aren't pointing the same element
+		if (currentJob->t1 < currentJob->t2){
+			*M1 = *currentJob; M1++; if (M1 > M2){ loop = false; }
+		}
+		else {
+			*M2 = *currentJob; M2--; if (M1 > M2){ loop = false; }
+		}
+		currentJob++;
+	}
+
+	for (int i = 0; i < nbJobs; i++){ //For every job, input the processing time on M1, M2
+		cout << "JobSchedule " << i << "|| t1 : " << jobSchedule[i].t1 << " t2 : " << jobSchedule[i].t2 << endl; //Ask for t2
+	}
+	system("pause");
 }
